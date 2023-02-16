5cee293947da159e9d39924d5e1610778a0ac9ac
Hillary sino <squidinfluencer@gmail.com>
2023-02-16 14:05:07 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A program that prints a line puts function
+ * Return: 0 (well done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
